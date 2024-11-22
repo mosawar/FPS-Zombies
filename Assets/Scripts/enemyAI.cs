1a1dcb66c30a8d4728bcea872593a406f793eb82
Julio232019 <lopezjulio2721@gmail.com>
2024-11-21 23:02:07 -0800

    Added zombie asset with animations

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -7,9 +7,12 @@ public class enemyAI : MonoBehaviour
 {
     NavMeshAgent nm;
     public Transform target;
-    public enum AIState{idle,chasing};
+    public float distanceThreshold = 10f;
+    public float attackThreshold = 8f;
+    public enum AIState{idle,chasing,attack};
 
     public AIState aiState = AIState.idle;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,26 +35,52 @@ public class enemyAI : MonoBehaviour
             {
                 case AIState.idle:
                     float dist = Vector3.Distance(target.position,transform.position);
-                    if (dist < 10f)
+                    if (dist < distanceThreshold)
                     {
                         aiState = AIState.chasing;
+                        animator.SetBool("Chasing", true);
                     }
                     nm.SetDestination(transform.position);
                     break;
                 case AIState.chasing:
                     dist = Vector3.Distance(target.position, transform.position);
-                    if(dist > 10f)
+                    
+                    if(dist > distanceThreshold)
                     {
                         aiState = AIState.idle;
+                        animator.SetBool("Chasing",false);
 
+                    }
+                    dist = Vector3.Distance(target.position, transform.position);
+                    Debug.Log(dist);
+                    if(dist < 2.7f)
+                    {
+                        Debug.Log("AttackThreshold met");
+
+                        //go into attack state
+                        aiState = AIState.attack;
+                        animator.SetBool("Attacking",true);
                     }
                     nm.SetDestination(target.position);
                     break;
+                case AIState.attack:
+                    Debug.Log("Attack");
+                    nm.SetDestination(transform.position);
+                    dist = Vector3.Distance(target.position, transform.position);
+                    if(dist > 2.7f)
+                    {
+                        aiState = AIState.chasing;
+                        animator.SetBool("Attacking", false);
+                        
+                    }
+                    break;
+                    
+                    //attack
                 default:
                     break;
             }
 
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(0.2f);
 
         }
     }
